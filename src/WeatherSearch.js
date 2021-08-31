463ec9690bd372490e43bd047e0fa1f2ae570d7d
Steph <stephbarron95@gmail.com>
2021-08-31 13:19:50 -0400

    fixed weather search

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,83 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          import React, {useState} from "react";
-          import axios from "axios";
-
-          export default function WeatherSearch() {
+export default function WeatherSearch() {
   const [city, setCity] = useState("");
-          const [loaded, setLoaded] = useState(false);
-          const [weather, setWeather] = useState({ });
-
-          function displayWeather(response) {
-            setLoaded(true);
-          setWeather({
-            temperature: response.data.main.temp,
-          wind: response.data.wind.speed,
-          humidity: response.data.main.humidity,
-          icon: `http://openweathermap.org/img/wn/${
-            response.data.weather[0].icon
-          }@2x.png`,
-          description: response.data.weather[0].description
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description
     });
   }
-
-          function handleSubmit(event) {
-            event.preventDefault();
-          let apiKey = "094780c710fa4efd669f0df8c3991927";
-          let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-          axios.get(apiUrl).then(displayWeather);
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
   }
-
-          function updateCity(event) {
-            setCity(event.target.value);
+  function updateCity(event) {
+    setCity(event.target.value);
   }
-
-          let form = (
-          <form onSubmit={handleSubmit}>
-            <input type="search" placeholder="Enter a city.." onChange={updateCity} />
-            <button type="Submit">Search</button>
-          </form>
-          );
-
-          if (loaded) {
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <button type="Submit">Search</button>
+    </form>
+  );
+  if (loaded) {
     return (
-          <div>
-            {form}
-            <ul>
-              <li>Temperature: {Math.round(weather.temperature)}°C</li>
-              <li>Description: {weather.description}</li>
-              <li>Humidity: {weather.humidity}%</li>
-              <li>Wind: {weather.wind}km/h</li>
-              <li>
-                <img src={weather.icon} alt={weather.description} />
-              </li>
-            </ul>
-          </div>
-          );
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
   } else {
     return form;
   }
 }
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
