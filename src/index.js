6bec81cbb77ea70219a4a1bcde41e904f50b7612
Steph <stephbarron95@gmail.com>
2021-09-01 07:13:24 -0400

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import axios from "axios";
-import App from './WeatherSearch';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 import WeatherSearch from "./WeatherSearch";
-
-import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <WeatherSearch />
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
 
 function App() {
   return (
