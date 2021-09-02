c736667cee10420833d9ea1942d2927389b43235
Steph <stephbarron95@gmail.com>
2021-09-02 11:40:33 -0400

    imported style

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./index.css";
 <WeatherTemperature celsius={weather.temperature} />
 
 export default function WeatherSearch() {
